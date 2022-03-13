9d8ede2a1050e90115a6e0e4b87eb6a2f5eb21e5
HimAYouN <63487132+HimAYouN@users.noreply.github.com>
2022-03-13 14:06:55 +0530

    Update preloder.js

--- jsFiles/preloder.js
+++ jsFiles/preloder.js
@@ -1,4 +1,4 @@
-window.addEventListener("load", ()=>{
+window.addEventListener("onLoad", ()=>{
     const preloader = document.querySelector(".preloader");
     preloader.classList.add("preloader-finish");
 })
